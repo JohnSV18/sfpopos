24dce0f0bdb9fb4a364acc52e2f2bc27f15912fe
John Marcos <johnsaguay@gmail.com>
2022-02-16 11:47:49 -0800

    dynamic content and footer

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import './Footer.css'
+function Footer() {
+    return (
+        <div className="Footer">
+            <p>John Marcos Copyright 2022</p>
+        </div>
+    )
+}
+
+export default Footer;
